fef5695c1aa27037f7065847505d60cf7b7bf285
Young Joong Kim <yjkimjunior@berkeley.edu>
2016-04-18 13:58:01 -0700

    resolved issue of post sizes

--- js/firebase_feed.js
+++ js/firebase_feed.js
@@ -1,36 +1,5 @@
-var baseURL = new Firebase("https://shufflemvp.firebaseio.com/");
-var postsRef = new Firebase("https://shufflemvp.firebaseio.com/posts");
-
-
-//TODO ***** Get the image from AWS S3
-function addPost(key, fullData) {
-	console.log(fullData);
-  $('#posts').append('<div class="col-md-4 market-item market-item-small">' + '<a href="">' + '<img class="img-responsive" src="img/food1.jpg" alt=""/>' + '</a>' + 
- '<h3>' + '<a href="" id="postName">' + fullData[key].title + '<a/>' + '</h3>' + 
- '<p id="description">' + fullData[key].description + '</p>' + '<button class="btn btn-primary offer">' + 'Make Offer' + '</button>' + 
- '<button class="btn btn-primary save" >' + 'Save' + '</button>' + '</div>');
-}
-
-/* This initially gets called for each post in the Firebase database
-* 	Looks a little like:
-* 		key = H2389HDAjucpiafo0
-*		childDatum = {title: dildos; description: sexy; minimumOffer: 20}
-*/
-function setUpFeed(key, fullData) {
-	addPost(key, fullData); 
-}
-
-$(document).ready(function() {
-	postsRef.once('value', function(snapshot) {
-		snapshot.forEach(function(childSnapshot) {
-			var key = childSnapshot.key();
-
-			var childDatum = childSnapshot.val();
-			// console.log(childData);
-			setUpFeed(key, snapshot.val());
-		});
-	})
-});
+var myFirebaseRef = new Firebase("https://shufflemvp.firebaseio.com/");
+var postsRef = myFirebaseRef.child('posts');
 
 $('input#productImage').on('change', function () {
   var reader = new FileReader();
@@ -49,13 +18,13 @@ $('input#productImage').on('change', function () {
 });
 
 function clearInputPage() {
-  $('div#productImageView').empty();
-  $('p#productName').empty();
-  $('p#productPrice').empty();
-  $('p#productDescription').empty();
+  $('p#productName').val("");
+  $('p#productPrice').val("");
+  $('p#productDescription').val("");
   $('input#productName').val("");
   $('input#minimumOffer').val("");
   $('input#productDescription').val("");
+  $('div#productImageView').empty();
 }
 
 $('input').on('change', function() {
@@ -75,8 +44,6 @@ $('#myModal').on('hide.bs.modal', function (e) {
   clearInputPage();
 });
 
-
-//User Pressed Sell button
 $('input#sellButton').click(function() {
   var date = new Date().toLocaleString()
 
@@ -93,13 +60,29 @@ $('input#sellButton').click(function() {
   $('#myModal').modal('hide');
 });
 
+
 //The post should now show up on the feed and in the database.
-//TODO ***** figure out how to save the url reference to Firebase, and actually save the image to AWS S3.
 postsRef.on('value', function(snapshot) {
-  var postsData = snapshot.val(); //an array of the post objects
-  var postIDs = Object.keys(postsData); //an array of each postID
-  addPost(postIDs[postIDs.length - 1], postsData);
+  var postsData = snapshot.val();
+  var postID = Object.keys(postsData);
+
+  
+
+
+
+  // console.log(postID[postID.length - 1]);
+
 });
+
+
+// postsRef.on('value', function(snapshot) {
+//  var postsData = snapshot.val();
+//  $('#posts').append('<div class="col-md-4 market-item market-item-small">' + '<a href="">' + '<img class="img-responsive" src="" alt=""/>' + '</a>' + 
+//  '<h3>' + '<a href="" id="postName">' + postsData.title + '<a/>' + '</h3>' + 
+//  '<p id="description">' + postsData.description + '</p>' + '</div>');
+
+// });
+
 
 
 //TODO ******************************
