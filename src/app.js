bcb20fa4ef3c734e17dee0d0e2ea94701a0f7dfd
Young Joong Kim <yjkimjunior@berkeley.edu>
2016-07-14 11:24:28 +0200

    initial skeleton setup

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+
+	constructor(props) {
+	  super();
+	}
+
+	render() {
+
+		return(
+			<section>
+
+			</section>
+			);
+	}
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
